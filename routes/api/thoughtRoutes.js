8992712fec5095c99369b28f3bbc439ddd3c767a
Andrea <keriazes.andrea@gmail.com>
2023-10-30 16:10:40 -0400

    completed routes folder

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -21,4 +21,11 @@ router
     .put(updateThought)
     .delete(deleteThought);
 
+// /api/thoughts/:thoughtId/reactions
+
+router
+    .route('/:thoughtId/reactions')
+    .post(addReaction)
+    .delete(deleteReaction);
+
 module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,3 +1,33 @@
+const router = require('express').Router();
+
+const {
+    getUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
+} = require('../../controllers/userController');
+
+// /api/users
+router
+    .route('/')
+    .get(getUsers)
+    .post(createUser);
+
+// /api/users/:id
+router
+    .route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router
+    .route('/:userId/friends/:friendId')
+    .post(addFriend)
+    .delete(deleteFriend);
+
+module.exports = router;
 // GET all users
 
 //GET a single user by its _id and populated thought and friend data
